67b1e73662c8d51b22f39ecad82e785839dbf68e
Deep Madhvani <deep.madhvani@csu.fullerton.edu>
2022-06-19 23:43:24 -0700

    First commit

---
+++ Deep Github/Deep Github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Deep Github
+//
+//  Created by csuftitan on 6/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Deep Github/Deep Github/Deep_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deep_GithubApp.swift
+//  Deep Github
+//
+//  Created by csuftitan on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Deep_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
